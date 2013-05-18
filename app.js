cd875a15cce2677449dc0e90761bfa6a86112b7c
David Miró <davidmirom@hotmail.com>
2013-05-18 08:18:53 -0400

    beta 2

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ var http = require('http'),
     bets = require('./lib/bets'),
     helper = require('./lib/helper'),
     url = require('url'),
-    route = require('router')();
+    route = require('router')(),
+    BETS_NAMES_TYPES = require('./lib/bets-types').BETS_NAMES_TYPES;
 
-var betsNameTypes = [];
-    betsNameTypes.primitiva = 'LAPR';
-    betsNameTypes.quiniela = 'LAQU';
-    
 var BET_TYPE_NOT_EXIST = 'Bet type does not exist';
 var INVALID_START_DATE = 'Start date is not a valid date';
 var INVALID_END_DATE = 'End date is not a valid date';
@@ -20,6 +17,11 @@ var error400 = function(res, error) {
     console.log(error);  
 };
 
+var error404 = function(res) {
+    res.writeHead(404);
+    res.end();  
+};
+
 var response200 = function(res, result) {
     res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
     res.write(JSON.stringify(result));
@@ -28,20 +30,39 @@ var response200 = function(res, result) {
 };
 
 //
+// favicon.ico
+//
+route.get('/favicon.ico', function(req, res) {   
+    error404(res);
+});
+  
+//
+// Bet types
+//
+route.get('/', function(req, res) {   
+    console.log('/ >',req.url);   
+    return response200(res, Object.keys(BETS_NAMES_TYPES)) ;   
+});
+
+//
 // One date
 //
 route.get('/{betNameType}/{date}', function(req, res) {
     
     console.log('/{betNameType}/{date} >',req.url);
+    
+    var betType = BETS_NAMES_TYPES[req.params.betNameType];
      
-    if (!betsNameTypes[req.params.betNameType]) {
+    if (!betType) {
         return error400(res, BET_TYPE_NOT_EXIST);
     }
+    
     var date = new Date(req.params.date);
     if (!helper.isValidDate(date)) {
         return error400(res, INVALID_DATE);
     }
-    bets.getBetsByDate(betsNameTypes[req.params.betNameType], date, date, function (error, result) {
+    
+    bets.getBetsByDate(betType, date, date, function (error, result) {
         if (error) {
             return error400(res, error);
         }
@@ -55,16 +76,18 @@ route.get('/{betNameType}/{date}', function(req, res) {
 route.get('/{betNameType}', function(req, res) {
 
     console.log('/{betNameType} >', req.url);
+    
+    var betType = BETS_NAMES_TYPES[req.params.betNameType];
 
-    if (!betsNameTypes[req.params.betNameType]) {
+    if (!betType) {
         return error400(res, BET_TYPE_NOT_EXIST);
     }
 
     var query = url.parse(req.url, true).query;
 
     // last bet
-    if (!Object.keys(query).length) {
-        bets.getLastBetPlayed(betsNameTypes[req.params.betNameType], function(error, result) {
+    if (Object.keys(query).length === 0) {
+        bets.getLastBetPlayed(betType, function(error, result) {
             if (error) {
                 return error400(res, error);
             }
@@ -85,14 +108,13 @@ route.get('/{betNameType}', function(req, res) {
             return error400(res, INVALID_END_DATE);
         }
 
-        bets.getBetsByDate(betsNameTypes[req.params.betNameType], startDate, endDate, function(error, result) {
+        bets.getBetsByDate(betType, startDate, endDate, function(error, result) {
             if (error) {
                 return error400(res, error);
             }
             return response200(res, result);
         });
     }
-
 });
 
 http.createServer(route).listen(process.env.PORT);
